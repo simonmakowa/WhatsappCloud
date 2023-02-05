e36b432c164e47656975bd971508528202e5548a
simonmakowa <smakowa03@gmail.com>
2023-02-05 16:01:37 +0200

    Initial Commit

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Webhook;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    //
+    public function index(Request $request){
+        $data = $request->all();
+        $challenge=$request->hub_challenge;
+        $dataToSTring = json_encode($data);
+        Webhook::create([
+            'webhook' => $dataToSTring,
+            'type' => '123'
+        ]);
+        return response ($challenge, 200);
+    }
+}
---
+++ database/migrations/2023_02_05_070209_create_webhooks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('webhooks', function (Blueprint $table) {
+            $table->id();
+            $table->string('type')->nullable();
+            $table->text('webhook')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('webhooks');
+    }
+};
